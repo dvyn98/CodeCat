0908335c8b60dc09d55afaeb945e82fa87404571
DIVYANSHU RAJPUT <divyanshurajputnbd@gmail.com>
2021-10-23 16:13:35 +0530

    Question 1

---
+++ week1/question1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+        printf("%d is a even no",a);
+    }
+        else
+     {   
+           printf("%d is a odd no",a);
+     } 
+    return 0;
+}
